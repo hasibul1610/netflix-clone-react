86b79cc95144c862b02ebb829b32fd2f7ac0ef92
hasibul1610 <bcse.hasibul@gmail.com>
2021-11-24 01:27:03 +0600

    first commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from '../../axios';
+import requests from '../../requests';
+
+
+const Banner = () => {
+    const [movie, setMovie] = useState([]);
+
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(request.data.results[Math.floor(Math.random() * request.data.results.length - 1)]);
+            // console.log(request.data.results[Math.floor(Math.random() * request.data.results.length - 1)]);
+        }
+        fetchData();
+    }, [])
+    console.log(movie);
+
+    return (
+        <header>
+            {/* Title */}
+
+        </header>
+    );
+};
+
+export default Banner;
